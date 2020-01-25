4fb15dd31efb6c34cc9b5fdac94694a4f907e084
Bogdan Mihaila <bogdan.mihaila@synopsys.com>
2020-01-25 13:30:49 +0200

    Fix c&p folder typo

--- Formula/krusader.rb
+++ Formula/krusader.rb
@@ -37,7 +37,7 @@ class Krusader < Formula
     qtpp = `#{Formula["qt"].bin}/qtpaths --plugin-dir`.chomp
     system "/usr/libexec/PlistBuddy",
       "-c", "Add :LSEnvironment:QT_PLUGIN_PATH string \"#{qtpp}\:#{HOMEBREW_PREFIX}/lib/qt5/plugins\"",
-      "#{bin}/dolphin.app/Contents/Info.plist"
+      "#{bin}/krusader.app/Contents/Info.plist"
   end
 
   def post_install
